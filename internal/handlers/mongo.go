540a9e10e95512e069cb73754e00292c45bbe505
Barkın Balcı <barkin.balci@gmail.com>
2024-12-13 20:19:22 +0300

    staging: mongodb endpoint

--- internal/handlers/mongo.go
+++ internal/handlers/mongo.go
@@ -22,38 +22,66 @@ func MongoHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var requestBody map[string]string
+	var requestBody map[string]interface{}
 	err = json.Unmarshal(body, &requestBody)
 	if err != nil {
 		http.Error(w, "Error unmarshalling request body", http.StatusBadRequest)
 		log.Printf("Error unmarshaling request body %v\n", err)
 		return
 	}
-	collectionName, ok := requestBody["collection_name"]
-	if !ok || collectionName == "" {
-		http.Error(w, "Missing collection name", http.StatusBadRequest)
-		log.Printf("Error missing collection_name %v\n", err)
+
+	startDate, ok := requestBody["startDate"].(string)
+	if !ok || startDate == "" {
+		http.Error(w, "Missing or invalid start date", http.StatusBadRequest)
+		log.Printf("Error missing or invalid start date %v\n", err)
+		return
+	}
+	endDate, ok := requestBody["endDate"].(string)
+	if !ok || endDate == "" {
+		http.Error(w, "Missing or invalid end date", http.StatusBadRequest)
+		log.Printf("Error missing or invalid end date %v\n", err)
 		return
 	}
 
-	mongoClient, err := mongo.NewMongoClient()
+	minCount, ok := requestBody["minCount"].(float64)
+	if !ok {
+		http.Error(w, "Missing or invalid minCount", http.StatusBadRequest)
+		log.Printf("Error missing or invalid minCount %v\n", err)
+		return
+	}
+
+	maxCount, ok := requestBody["maxCount"].(float64)
+	if !ok {
+		http.Error(w, "Missing or invalid maxCount", http.StatusBadRequest)
+		log.Printf("Error missing or invalid maxCount %v\n", err)
+		return
+	}
+
+	mongoClient, err := mongo.NewClient()
 	if err != nil {
 		http.Error(w, "Error connecting to MongoDB", http.StatusInternalServerError)
-		log.Printf("Error creating mongo client, %v \n", err)
+		log.Printf("Error connecting to MongoDB %v\n", err)
 		return
 	}
+	records, err := mongoClient.FetchData(startDate, endDate, int(minCount), int(maxCount))
 
-	results, err := mongoClient.FetchData(collectionName)
 	if err != nil {
-		http.Error(w, "Error fetching data", http.StatusInternalServerError)
-		log.Printf("Error Fetching data %v\n", err)
-		return
+		if err.Error() == "invalid start date format" || err.Error() == "invalid end date format" {
+			records = &mongo.FilteredRecords{
+				Code:    1,
+				Msg:     err.Error(),
+				Records: []mongo.Record{},
+			}
+		} else {
+			http.Error(w, "Error fetching data from MongoDB", http.StatusInternalServerError)
+			log.Printf("Error fetching data from MongoDB %v \n", err)
+			return
+		}
 	}
-
 	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(results)
+	err = json.NewEncoder(w).Encode(records)
 	if err != nil {
-		http.Error(w, "Error marshaling data", http.StatusInternalServerError)
+		http.Error(w, "Error encoding data", http.StatusInternalServerError)
 		return
 	}
 }
--- pkg/mongo/mongo.go
+++ pkg/mongo/mongo.go
@@ -12,63 +12,135 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type DBClient struct {
+type Client struct {
 	client *mongo.Client
 	db     *mongo.Database
 }
 
-func NewMongoClient() (*DBClient, error) {
+func NewClient() (*Client, error) {
 	mongoURI := os.Getenv("MONGODB_URI")
 	if mongoURI == "" {
 		return nil, fmt.Errorf("MONGO_URI environment variable is not set")
 	}
 
 	clientOptions := options.Client().ApplyURI(mongoURI)
-
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error connecting to MongoDB: %w", err)
 	}
 
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error pinging MongoDB: %w", err)
 	}
 
-	dbName := "development"
-	db := client.Database(dbName)
-	log.Printf("Connected to MongoDB: %s\n", mongoURI)
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		return nil, fmt.Errorf("DB_NAME environment variable is not set")
+	}
 
-	return &DBClient{client: client, db: db}, nil
+	db := client.Database(dbName)
+	log.Printf("Connected to MongoDB: %s, database: %s\n", mongoURI, dbName)
+
+	return &Client{client: client, db: db}, nil
 }
 
-func (m *DBClient) FetchData(collectionName string) ([]bson.M, error) {
+type Record struct {
+	Key        string    `bson:"key" json:"key"`
+	CreatedAt  time.Time `bson:"createdAt" json:"createdAt"`
+	TotalCount int       `json:"totalCount"`
+}
+
+type FilteredRecords struct {
+	Code    int      `json:"code"`
+	Msg     string   `json:"msg"`
+	Records []Record `json:"records"`
+}
+
+func (m *Client) FetchData(startDate, endDate string, minCount, maxCount int) (*FilteredRecords, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	collection := m.db.Collection(collectionName)
+	collection := m.db.Collection("records")
 
-	cursor, err := collection.Find(ctx, bson.M{})
+	startTime, err := time.Parse("2006-01-02", startDate)
 	if err != nil {
-		log.Printf("Error fetching data from MongoDB, %v \n", err)
-		return nil, err
+		log.Printf("Error parsing start date: %v\n", err)
+		return nil, fmt.Errorf("invalid start date format")
 	}
 
+	endTime, err := time.Parse("2006-01-02", endDate)
+	if err != nil {
+		log.Printf("Error parsing end date: %v\n", err)
+		return nil, fmt.Errorf("invalid end date format")
+	}
+
+	pipeline := []bson.M{
+		{
+			"$match": bson.M{
+				"createdAt": bson.M{
+					"$gte": startTime,
+					"$lte": endTime,
+				},
+			},
+		},
+		{
+			"$addFields": bson.M{
+				"totalCount": bson.M{"$sum": "$counts"},
+			},
+		},
+		{
+			"$match": bson.M{
+				"totalCount": bson.M{
+					"$gte": minCount,
+					"$lte": maxCount,
+				},
+			},
+		},
+		{
+			"$project": bson.M{
+				"_id":        0,
+				"key":        1,
+				"createdAt":  1, // Keeping 'createdAt' as it is already a date type
+				"totalCount": 1,
+			},
+		},
+	}
+
+	cursor, err := collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		log.Printf("Error fetching data from MongoDB: %v\n", err)
+		return nil, fmt.Errorf("error fetching data from MongoDB: %w", err)
+	}
 	defer func(cursor *mongo.Cursor, ctx context.Context) {
 		err := cursor.Close(ctx)
 		if err != nil {
-			log.Printf("Error fetching data from MongoDB, %v \n", err)
+			log.Printf("Error closing cursor: %v\n", err)
 		}
 	}(cursor, ctx)
 
-	var results []bson.M
-
-	if err = cursor.All(ctx, &results); err != nil {
-		log.Printf("Error decoding data from MongoDB, %v \n", err)
-		return nil, err
+	var results []Record
+	for cursor.Next(ctx) {
+		var record Record
+		if err := cursor.Decode(&record); err != nil {
+			log.Printf("Error decoding record: %v\n", err)
+			return nil, fmt.Errorf("error decoding record from mongo: %w", err)
+		}
+		results = append(results, record)
 	}
-	return results, nil
+
+	if err := cursor.Err(); err != nil {
+		log.Printf("Error during cursor iteration: %v\n", err)
+		return nil, fmt.Errorf("error during cursor iteration: %w", err)
+	}
+
+	filteredResponse := &FilteredRecords{
+		Code:    0,
+		Msg:     "Success",
+		Records: results,
+	}
+	return filteredResponse, nil
 }
